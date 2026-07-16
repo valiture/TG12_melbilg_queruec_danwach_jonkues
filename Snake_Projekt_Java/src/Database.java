c12d408cfaca6d8bd172c7b1df2d09e17d970980
valiture <postconscious1984@gmail.com>
2026-07-16 18:15:17 +0200

    Remove password from Database connection
    Removed the password from the Database class for security reasons.

--- Snake_Projekt_Java/src/Database.java
+++ Snake_Projekt_Java/src/Database.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class Database {
     private static final String URL = "jdbc:mysql://localhost:3306/snake_game";
     private static final String USER = "root";
-    private static final String PASSWORD = "Melihistcool81";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
