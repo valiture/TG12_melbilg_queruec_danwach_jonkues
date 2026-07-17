350630814eb928d7f37edad384e052ea50a2b33c
valiture <postconscious1984@gmail.com>
2026-07-17 07:40:01 +0200

    Delete Code directory

--- Code/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Spielfeld spielfeld = new Spielfeld(10, 5);
-        Snake_Original snakeOriginal = new Snake_Original(3, spielfeld);
-
-    }
-}
